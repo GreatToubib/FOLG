ec3881a71ec47ec679f1a6a61292e87f57490d37
Thomas Guily <ThomasGuily@users.magellan.fpms.ac.be>
2020-11-19 15:34:16 +0100

    softmax function and costfunction implemented

--- Guily/Main.m
+++ Guily/Main.m
@@ -1,3 +1,13 @@
 %wow le projet 
-[m,p] = size(Xts)
-w = ones (m)
+[m,p] = size(Xts);
+W = ones (20,m) * 0.01 ; 
+
+a = W * Xts (:,1) ;
+
+a = softmax(a) ;
+
+y_encoded = (yts==1:20);
+true = y_encoded (1);
+
+cost = costfunction (a, true);
+
---
+++ Guily/softmax.m
@@ -0,0 +1,15 @@
+function [zs] = softmax(z)
+  
+  zs = zeros (1,20);
+  somme = 0;
+  for i = 1:size(z)
+   
+  zs(i) = e**z(i);
+  somme += e**z(i)  ;
+  end
+  
+  
+  
+  zs = zs./somme ; 
+  
+end
