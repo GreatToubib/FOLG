17e0b52cf86d787c87c6007cd0d30ba6cb7d3697
GreatToubib <basilburleon@hotmail.com>
2020-12-07 16:15:07 +0100

    GD not working lel, gotta matricize

--- Guily/GD.m
+++ Guily/GD.m
@@ -7,29 +7,34 @@ for epoch = 1:epoch_number
         Xts_row = Xts(:,i);
         ytrue = y_encoded (i,:);
         ypred = W * Xts_row ; % ypred avant activation du test sample
-
+        size(ypred)
         if activation =='sigmoid'
             a_ypred = sigmoid(ypred);  % ypred avec activation sigmoid
             ader = sigmoidder (ypred);
         end
-        if activation =='softmax'
-            a_ypred = softmax(ypred) ;
-            ader = softmaxder(ypred) ;
-        end
+        %if activation =='softmax'
+            %a_ypred = softmax(ypred) ;
+            %ader = softmaxder(ypred) ;
+        %end
         %if activation =='relu'
             %a_ypred = ReLU (ypred)
             %ader = ReLUder (ypred)
         %end
+        size(a_ypred)
         
         cost = cost + costfunction(a_ypred, ytrue); % test de la cost function sur le test sample
         
         part1 = a_ypred-ytrue;
+        size(part1)
         part2 = ader;
+        size(part2)
         part3=  (part1' .*  part2);
+        size(part3)
+        Xts_row=Xts_row';
+        size(Xts_row)
         gd = Xts_row * ( 1/20 * part3)';
         W = W - lr*gd;
        
-
     end
     if epoch == 1 
         previous_cost = 10000;
